f648cddab53cbf96bfc61a0625b30f4f6d5bbff9
Enzslv4 <enzosilva1420002@gmail.com>
2026-04-25 22:45:09 -0300

    teste

--- lumen/views.py
+++ lumen/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from apps.accounts import models
+import os
 
 def home(request):
     return render(request, 'home.html')
@@ -8,9 +9,9 @@ def create_super(request):
     from models import User
     User.objects.filter(username='admin_lumen').delete()
     User.objects.create_superuser(
-        username='admin_lumen',
-        email='seu@email.com',
-        password='sua_nova_senha'
+        username=os.getenv('SUPERUSER_NAME'),
+        email=os.getenv('SUPERUSER_EMAIL'),
+        password=os.getenv('SUPERUSER_PASSW')
     )
     from django.http import HttpResponse
     return HttpResponse('Superusuário criado com sucesso!')
