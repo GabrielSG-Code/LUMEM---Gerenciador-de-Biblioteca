4861e708eb9a8e8b7ea2ca71195d5bd8bde3f7b9
Enzslv4 <enzosilva1420002@gmail.com>
2026-05-05 21:17:51 -0300

    alterando allowed hosts para aceitar o novo link vinculado ao repo de gabriel

--- lumen/settings.py
+++ lumen/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["lumem-gerenciador-de-biblioteca.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["lumen-a5cs.onrender.com", "127.0.0.1"]
 
 
 # Application definition
