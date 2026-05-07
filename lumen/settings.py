c1bd9adaa4d7a7400e0e64a95365d5ac4947a109
thiago <aluadotiago@gmail.com>
2026-05-06 23:05:11 -0300

    Att.

--- lumen/settings.py
+++ lumen/settings.py
@@ -23,19 +23,11 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ["lumen-a5cs.onrender.com", "127.0.0.1"]
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -80,8 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'lumen.wsgi.application'
 
-# Database
-
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
@@ -90,7 +80,6 @@ DATABASES = {
 }
 
 DATABASE_URL = os.getenv('DATABASE_URL', '')
-
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(
@@ -106,10 +95,8 @@ else:
         }
     }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -125,22 +112,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
